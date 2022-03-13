2b14af58b6e33b942e61ed427d45e5494f689351
chyu <1163706779@qq.com>
2022-03-13 11:40:47 +0800

    写一道简单dfs

--- dfs.js
+++ dfs.js
@@ -95,3 +95,40 @@
         }
     };
 }
+{
+    // 100. 相同的树
+    // 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+
+    // 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+    /**
+     * Definition for a binary tree node.
+     * function TreeNode(val, left, right) {
+     *     this.val = (val===undefined ? 0 : val)
+     *     this.left = (left===undefined ? null : left)
+     *     this.right = (right===undefined ? null : right)
+     * }
+     */
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    var isSameTree = function(p, q) {
+        var ret = true
+        function dfs(root1, root2){
+            if(!ret || !root1 && !root2) return
+            if(!root1 && root2 || !root2 && root1){
+                ret = false
+                return
+            }
+            dfs(root1.left, root2.left)
+            if(root1.val != root2.val){
+                ret = false
+                return
+            }
+            dfs(root1.right, root2.right)
+        }
+        dfs(p, q)
+        return ret
+    };
+}
