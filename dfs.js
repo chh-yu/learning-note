74f2b7fb746a8ed6882ebdfab671fe7d7fe9c4d2
chyu <1163706779@qq.com>
2022-03-15 11:33:33 +0800

    小刷一手dfs

--- dfs.js
+++ dfs.js
@@ -269,3 +269,53 @@
         return dfs(root)
     };
 }
+{
+    // 112. 路径总和
+    // 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。如果存在，返回 true ；否则，返回 false 。
+    // 叶子节点 是指没有子节点的节点。
+    /**
+     * Definition for a binary tree node.
+     * function TreeNode(val, left, right) {
+     *     this.val = (val===undefined ? 0 : val)
+     *     this.left = (left===undefined ? null : left)
+     *     this.right = (right===undefined ? null : right)
+     * }
+     */
+    /**
+     * @param {TreeNode} root
+     * @param {number} targetSum
+     * @return {boolean}
+     */
+    var hasPathSum = function(root, targetSum) {
+        if(!root) return false
+        var check = false
+        function dfs(root){
+            let possible = [] 
+            if(!root) return [0]
+            if(!root.left && !root.right) return [root.val]
+            if(!root.left) {
+                dfs(root.right).forEach((itm)=>{
+                    possible.push(root.val+itm)
+                })
+                return possible
+            }
+            if(!root.right) {
+                dfs(root.left).forEach((itm)=>{
+                    possible.push(root.val+itm)
+                })
+                return possible
+            }
+            dfs(root.right).forEach((itm)=>{
+                possible.push(root.val+itm)
+            })
+            dfs(root.left).forEach((itm)=>{
+                possible.push(root.val+itm)
+            })
+            return possible
+        }
+        dfs(root).forEach((itm)=>{
+            if(itm == targetSum) check = true
+        })
+        return check
+    };
+}
